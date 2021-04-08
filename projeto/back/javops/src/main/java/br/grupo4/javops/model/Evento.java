e1c8e59ecf770f551c3e2ad38cc9dc5431ecd1cb
AlexandrePugles <alexandre.pugles@gmail.com>
2021-04-08 17:37:07 -0300

    tudo

--- projeto/back/javops/src/main/java/br/grupo4/javops/model/Evento.java
+++ projeto/back/javops/src/main/java/br/grupo4/javops/model/Evento.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,5 +22,45 @@ public class Evento {
 	
 	@Column(name="data_evt", nullable=false)
 	private LocalDate dataEvt;
+
+	@OneToOne
+	@JoinColumn(name="id_alarme")
+	private Alarme alarme;
+
+	@OneToOne
+	@JoinColumn(name="id_equip")
+	private Equipamento equipamento;
+
+	public int getNumSeq() {
+		return numSeq;
+	}
+
+	public void setNumSeq(int numSeq) {
+		this.numSeq = numSeq;
+	}
+
+	public LocalDate getDataEvt() {
+		return dataEvt;
+	}
+
+	public void setDataEvt(LocalDate dataEvt) {
+		this.dataEvt = dataEvt;
+	}
+
+	public Alarme getAlarme() {
+		return alarme;
+	}
+
+	public void setAlarme(Alarme alarme) {
+		this.alarme = alarme;
+	}
+
+	public Equipamento getEquipamento() {
+		return equipamento;
+	}
+
+	public void setEquipamento(Equipamento equipamento) {
+		this.equipamento = equipamento;
+	}
 
 }
